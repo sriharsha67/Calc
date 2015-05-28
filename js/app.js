1748837ec9726d93c3dd08299d0997062822274d
sriharsha67 <sriharsha67@gmail.com>
2015-05-28 12:35:55 +0530

    Functionality for special cases such as DEL,=,AC

--- js/app.js
+++ js/app.js
@@ -1,15 +1,36 @@
-function handleKeyPress(key){
-  if (["AC","DEL","="].indexOf($(key).text()) == -1) {
-    $('#preview').html(
-      $('#preview').html() + $(key).text()
+Calc = {
+  clear: function() {
+    $('#preview').html("");
+    $('#result').html("0");    
+  },
+  deleteCharFromPreview: function() {
+    var preview = $('#preview').html();
+    $('#preview').html(preview.slice(0, preview.length-1));
+  },
+  calculateResult: function() {
+    $('#result').html(eval($('#preview').html()));
+  },
+  handleKeyPress: function(key) {
+    switch($(key).text()) {
+    case "AC":
+      Calc.clear();
+      break;
+    case "DEL":
+      Calc.deleteCharFromPreview();
+      break;
+    case "=":
+      Calc.calculateResult();
+      break;
+    default:
+      $('#preview').html(
+        $('#preview').html() + $(key).text()
       );
-  } else {
-    console.log($(key).text());
+    }
   }
-}
+};
 
-$(document).ready(function(){
+$(document).ready(function() {
   $('.key').click(function(event){
-    handleKeyPress(this);
+    Calc.handleKeyPress(this);
   });
 });
