55379472bc25159925999a2ba243c6bebeddd1f1
sriharsha67 <sriharsha67@gmail.com>
2015-05-28 11:46:52 +0530

    Handle Key Press

---
+++ js/app.js
@@ -0,0 +1,15 @@
+function handleKeyPress(key){
+  if (["AC","DEL","="].indexOf($(key).text()) == -1) {
+    $('#preview').html(
+      $('#preview').html() + $(key).text()
+      );
+  } else {
+    console.log($(key).text());
+  }
+}
+
+$(document).ready(function(){
+  $('.key').click(function(event){
+    handleKeyPress(this);
+  });
+});
